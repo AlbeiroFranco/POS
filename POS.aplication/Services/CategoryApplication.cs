dea774cd4de021892be05798ec3b852fd1a01b41
AlbeiroFranco <jose.arleck91@hotmail.com>
2023-01-07 16:56:31 -0500

    Feat: se agregan interfaces Dtos y servicio

---
+++ POS.aplication/Services/CategoryApplication.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using POS.Aplication.Commons.Bases;
+using POS.Aplication.Dtos.Request;
+using POS.Aplication.Dtos.Response;
+using POS.Aplication.Interfaces;
+using POS.Aplication.Validators.Category;
+using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Commons.Bases.Response;
+using POS.Infrastructure.Persistences.Interfaces;
+
+namespace POS.Aplication.Services
+{
+    public class CategoryApplication : ICategoryApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly CategoryValidator _validationRules;
+
+        public CategoryApplication(IUnitOfWork unitOfWork, IMapper mapper, CategoryValidator validationRules)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validationRules = validationRules;
+        }
+
+        public Task<BaseResponse<IEnumerable<CategorySelectResponseDto>>> ListSelectCategories()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<CategoryResponseDto>> CategoryById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<bool>> RegisterCategory(CategoryRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<BaseEntityResponse<CategoryResponseDto>>> ListCategories(BaseFiltersRequest filters)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<bool>> RemoveCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ POS.aplication/Validators/Category/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using POS.Aplication.Dtos.Request;
+
+namespace POS.Aplication.Validators.Category
+{
+    public class CategoryValidator : AbstractValidator<CategoryRequestDto>
+    {
+        public CategoryValidator() 
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("El campo de Nombre no puede ser nulo.")
+                .NotEmpty().WithMessage("El campo de nombre no puede ser vacio");
+        }
+    }
+}
