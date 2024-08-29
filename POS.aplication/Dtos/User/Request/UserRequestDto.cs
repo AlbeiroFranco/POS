d64b86a31fadb22ad53c00653772ceec8eb85c9e
AlbeiroFranco <87336609+AlbeiroFranco@users.noreply.github.com>
2024-08-28 20:19:43 -0500

    Add prop userId in dto

--- POS.aplication/Dtos/User/Request/UserRequestDto.cs
+++ POS.aplication/Dtos/User/Request/UserRequestDto.cs
@@ -4,11 +4,11 @@ namespace POS.Aplication.Dtos.User.Request
 {
     public class UserRequestDto
     {
+        public int UserId { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }
         public string? Email { get; set; }
         public IFormFile? Image { get; set; }
         public int? State { get; set; }
-        
     }
 }
